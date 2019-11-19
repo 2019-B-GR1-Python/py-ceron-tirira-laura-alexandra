48e8d4224de52eff8671c361d100cee5da8daf1f
lceron2010 <laura.ceron@epn.edu.ec>
2019-11-19 09:00:47 -0500

    pandas-introduccion-series

---
+++ 03-pandas/A_introduccion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+nombre="laura"
+numero=3
+lista=[1,2,3]
+tupla=(4,5,6)
+diccionario={
+        "nombre":"alexandra",
+        "cedula":"12"
+             }
